d76b55bef8caaad3c604927bc35bd64c35e8f787
Eric Mill <eric.mill@gsa.gov>
2015-07-24 19:39:05 -0400

    allow a URL to be given for a CSV (even a gzipped one)

--- scanners/analytics.py
+++ scanners/analytics.py
@@ -2,6 +2,7 @@
 from scanners import utils
 import logging
 import os
+import requests
 
 ###
 # == analytics ==
@@ -16,14 +17,34 @@ def init(options):
     global analytics_domains
 
     analytics_file = options.get("analytics")
-    if ((not analytics_file) or
-            (not analytics_file.endswith(".csv")) or
-            (not os.path.exists(analytics_file))):
-        no_csv = "--analytics should point to a CSV of participating domains."
+    if (not analytics_file) or (not analytics_file.endswith(".csv")):
+        no_csv = "--analytics should point to the file path or URL to a CSV of participating domains."
         logging.error(no_csv)
         return False
 
-    analytics_domains = utils.load_domains(analytics_file)
+    # It's a URL, download it first.
+    if analytics_file.startswith("http:") or analytics_file.startswith("https:"):
+
+        analytics_path = os.path.join(utils.cache_dir(), "analytics.csv")
+
+        try:
+            response = requests.get(analytics_file)
+            utils.write(response.text, analytics_path)
+        except:
+            no_csv = "--analytics URL not downloaded successfully."
+            logging.error(no_csv)
+            return False
+
+    # Otherwise, read it off the disk
+    else:
+        analytics_path = analytics_file
+
+        if (not os.path.exists(analytics_path)):
+            no_csv = "--analytics file not found."
+            logging.error(no_csv)
+            return False
+
+    analytics_domains = utils.load_domains(analytics_path)
 
     return True
 
