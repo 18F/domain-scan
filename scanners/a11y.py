f32d69cacc55d35e3da2c4266c023cd55eeaf285
Alan deLevie <adelevie@gmail.com>
2015-09-02 13:33:03 -0400

    Collect counts of a11y notices, errors, and warnings

--- scanners/a11y.py
+++ scanners/a11y.py
@@ -5,6 +5,7 @@ import os
 
 import shlex
 from subprocess import Popen, PIPE
+import json
 
 def get_exitcode_stdout_stderr(cmd):
     """
@@ -19,23 +20,32 @@ def get_exitcode_stdout_stderr(cmd):
     return exitcode, out, err
 
 
-
-
 command = os.environ.get("PA11Y_PATH", "pa11y")
 workers = 1
 
+PA11Y_STANDARD = 'WCAG2AA'
+
 def scan(domain, options):
-    command = "pa11y --reporter csv %s" % domain
-
-    cmd = command
-    exitcode, out, err = get_exitcode_stdout_stderr(cmd)
-
-    print(out)
-
-    yield [1,2,3]
+    command = "pa11y --standard %s --reporter json %s" % (PA11Y_STANDARD, domain)
+    exitcode, out, err = get_exitcode_stdout_stderr(command)
+    
+    reports = json.loads(out.decode("utf-8"))
+    
+    notices = len([r for r in reports if r['type'] == 'notice'])
+    errors = len([r for r in reports if r['type'] == 'error'])
+    warnings = len([r for r in reports if r['type'] == 'warning'])
+        
+    yield [
+        PA11Y_STANDARD,
+        warnings,
+        errors,
+        notices
+    ]
+    
 
 headers = [
-    "Errors",
+    "Standard",
     "Warnings",
+    "Errors",
     "Notices"
 ]
