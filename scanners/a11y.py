39b1868b05c7445f299b79394829f96c7f170534
Alan deLevie <adelevie@gmail.com>
2016-03-16 18:06:35 -0400

    Modernized and made functional a11y.py

--- scanners/a11y.py
+++ scanners/a11y.py
@@ -3,28 +3,9 @@ from scanners import utils
 import json
 import os
 
-import shlex
-from subprocess import Popen, PIPE
-import json
-
-def get_exitcode_stdout_stderr(cmd):
-    """
-    Execute the external command and get its exitcode, stdout and stderr.
-    """
-    args = shlex.split(cmd)
-
-    proc = Popen(args, stdout=PIPE, stderr=PIPE)
-    out, err = proc.communicate()
-    exitcode = proc.returncode
-
-    return exitcode, out, err
-
-
-command = os.environ.get("PA11Y_PATH", "pa11y")
 workers = 1
-
 PA11Y_STANDARD = 'WCAG2AA'
-
+pa11y = os.environ.get("PA11Y_PATH", "pa11y")
 headers = [
     "type",
     "typeCode",
@@ -35,14 +16,20 @@ headers = [
 ]
 
 def scan(domain, options):
-    print(options)
-    command = "pa11y --standard %s --reporter json %s" % (PA11Y_STANDARD, domain)
-    exitcode, out, err = get_exitcode_stdout_stderr(command)
-    
-    reports = json.loads(out.decode("utf-8"))
+    logging.debug("[%s][a11y]" % domain)
+    # The '--level none' piece here is crucial.
+    # By default, pa11y will return with a non-zero exit code if at least one
+    # error is detected in the scan. Setting '--level none'
+    command = [pa11y, domain, "--reporter", "json", "--standard", PA11Y_STANDARD, "--level", "none"]
+    raw = utils.scan(command)
 
-    for report in reports:
-        result = []
-        for header in headers:
-            result.append(report.get(header))
-        yield result
+    results = json.loads(raw)
+    for data in results:
+        yield [
+            data['type'],
+            data['typeCode'],
+            data['code'],
+            data['message'],
+            data['context'],
+            data['selector']
+        ]
