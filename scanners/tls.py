c37fd16b59e66b2250c5db8d23d91e41d2ba6581
Eric Mill <eric@konklone.com>
2016-08-28 01:16:15 -0400

    no need to indent under else

--- scanners/tls.py
+++ scanners/tls.py
@@ -25,94 +25,94 @@ def scan(domain, options):
         logging.debug("\tSkipping, HTTPS not supported in inspection.")
         return None
 
+
+    # cache reformatted JSON from ssllabs
+    cache = utils.cache_path(domain, "tls")
+
+    force = options.get("force", False)
+
+    if (force is False) and (os.path.exists(cache)):
+        logging.debug("\tCached.")
+        raw = open(cache).read()
+        data = json.loads(raw)
+
+        if data.get('invalid'):
+            return None
     else:
-        # cache reformatted JSON from ssllabs
-        cache = utils.cache_path(domain, "tls")
+        logging.debug("\t %s %s" % (command, domain))
 
-        force = options.get("force", False)
+        usecache = str(not force).lower()
 
-        if (force is False) and (os.path.exists(cache)):
-            logging.debug("\tCached.")
-            raw = open(cache).read()
+        if options.get("debug"):
+            cmd = [command, "--usecache=%s" % usecache,
+                   "--verbosity=debug", domain]
+        else:
+            cmd = [command, "--usecache=%s" % usecache,
+                   "--quiet", domain]
+        raw = utils.scan(cmd)
+        if raw:
             data = json.loads(raw)
 
-            if data.get('invalid'):
+            # if SSL Labs gave us back an error response, cache this
+            # as an invalid entry.
+            if len(data) < 1:
+                utils.write(utils.invalid({'response': data}), cache)
                 return None
+
+            # we only give ssllabs-scan one at a time,
+            # so we can de-pluralize this
+            data = data[0]
+
+            # if SSL Labs had an error hitting the site, cache this
+            # as an invalid entry.
+            if data["status"] == "ERROR":
+                utils.write(utils.invalid(data), cache)
+                return None
+
+            utils.write(utils.json_for(data), cache)
         else:
-            logging.debug("\t %s %s" % (command, domain))
+            return None
+            # raise Exception("Invalid data from ssllabs-scan: %s" % raw)
 
-            usecache = str(not force).lower()
+    # can return multiple rows, one for each 'endpoint'
+    for endpoint in data['endpoints']:
 
-            if options.get("debug"):
-                cmd = [command, "--usecache=%s" % usecache,
-                       "--verbosity=debug", domain]
-            else:
-                cmd = [command, "--usecache=%s" % usecache,
-                       "--quiet", domain]
-            raw = utils.scan(cmd)
-            if raw:
-                data = json.loads(raw)
+        # this meant it couldn't connect to the endpoint
+        if not endpoint.get("grade"):
+            continue
 
-                # if SSL Labs gave us back an error response, cache this
-                # as an invalid entry.
-                if len(data) < 1:
-                    utils.write(utils.invalid({'response': data}), cache)
-                    return None
+        sslv3 = False
+        tlsv12 = False
+        for protocol in endpoint['details']['protocols']:
+            if ((protocol['name'] == "SSL") and
+                    (protocol['version'] == '3.0')):
+                sslv3 = True
+            if ((protocol['name'] == "TLS") and
+                    (protocol['version'] == '1.2')):
+                tlsv12 = True
 
-                # we only give ssllabs-scan one at a time,
-                # so we can de-pluralize this
-                data = data[0]
+        spdy = False
+        h2 = False
+        npn = endpoint['details'].get('npnProtocols', None)
+        if npn:
+            spdy = ("spdy" in npn)
+            h2 = ("h2" in npn)
 
-                # if SSL Labs had an error hitting the site, cache this
-                # as an invalid entry.
-                if data["status"] == "ERROR":
-                    utils.write(utils.invalid(data), cache)
-                    return None
-
-                utils.write(utils.json_for(data), cache)
-            else:
-                return None
-                # raise Exception("Invalid data from ssllabs-scan: %s" % raw)
-
-        # can return multiple rows, one for each 'endpoint'
-        for endpoint in data['endpoints']:
-
-            # this meant it couldn't connect to the endpoint
-            if not endpoint.get("grade"):
-                continue
-
-            sslv3 = False
-            tlsv12 = False
-            for protocol in endpoint['details']['protocols']:
-                if ((protocol['name'] == "SSL") and
-                        (protocol['version'] == '3.0')):
-                    sslv3 = True
-                if ((protocol['name'] == "TLS") and
-                        (protocol['version'] == '1.2')):
-                    tlsv12 = True
-
-            spdy = False
-            h2 = False
-            npn = endpoint['details'].get('npnProtocols', None)
-            if npn:
-                spdy = ("spdy" in npn)
-                h2 = ("h2" in npn)
-
-            yield [
-                endpoint['grade'],
-                endpoint['details']['cert']['sigAlg'],
-                endpoint['details']['key']['alg'],
-                endpoint['details']['key']['size'],
-                endpoint['details']['forwardSecrecy'],
-                endpoint['details']['ocspStapling'],
-                endpoint['details'].get('fallbackScsv', "N/A"),
-                endpoint['details']['supportsRc4'],
-                sslv3,
-                tlsv12,
-                spdy,
-                endpoint['details']['sniRequired'],
-                h2
-            ]
+        yield [
+            endpoint['grade'],
+            endpoint['details']['cert']['sigAlg'],
+            endpoint['details']['key']['alg'],
+            endpoint['details']['key']['size'],
+            endpoint['details']['forwardSecrecy'],
+            endpoint['details']['ocspStapling'],
+            endpoint['details'].get('fallbackScsv', "N/A"),
+            endpoint['details']['supportsRc4'],
+            sslv3,
+            tlsv12,
+            spdy,
+            endpoint['details']['sniRequired'],
+            h2
+        ]
 
 headers = [
     "Grade",  # unique to SSL Labs
