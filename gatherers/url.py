f78e06c416fba5335df8908406be377977707212
Eric Mill <eric@konklone.com>
2016-09-07 17:46:04 -0500

    allow the url gatherer to take a local path

--- gatherers/url.py
+++ gatherers/url.py
@@ -7,25 +7,32 @@ import logging
 #
 # Gathers hostnames from a CSV at a given URL.
 #
-# --url: The URL to download. Will be parsed as a CSV.
+# --url: The URL to download. Can also be a local path.
+#        Will be parsed as a CSV.
 #
 
 def gather(suffix, options):
     url = options.get("url")
     if url is None:
-        logging.warn("A --url is required.")
+        logging.warn("A --url is required. (Can be a local path.)")
         exit(1)
 
-    # Though it's saved in cache/, it will be downloaded every time.
-    remote_path = os.path.join(utils.cache_dir(), "url.csv")
+    # remote URL
+    if url.startswith("http:") or url.startswith("https:"):
+        # Though it's saved in cache/, it will be downloaded every time.
+        remote_path = os.path.join(utils.cache_dir(), "url.csv")
 
-    try:
-        response = requests.get(url)
-        utils.write(response.text, remote_path)
-    except:
-        logging.error("Remote URL not downloaded successfully.")
-        print(utils.format_last_exception())
-        exit(1)
+        try:
+            response = requests.get(url)
+            utils.write(response.text, remote_path)
+        except:
+            logging.error("Remote URL not downloaded successfully.")
+            print(utils.format_last_exception())
+            exit(1)
+
+    # local path
+    else:
+        remote_path = url
 
     for domain in utils.load_domains(remote_path):
         yield domain
