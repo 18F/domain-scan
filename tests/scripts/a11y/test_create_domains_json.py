04ed5554d7a37aa5f4c504e4f59f3c1ef8726b51
Alan deLevie <adelevie@gmail.com>
2016-09-13 16:34:40 -0400

    stopping point

--- tests/scripts/a11y/test_create_domains_json.py
+++ tests/scripts/a11y/test_create_domains_json.py
@@ -1,12 +1,34 @@
 import unittest
+import json
 
 from scripts.a11y.create_domains_file import CreateDomainsJSON
 
 class TestCreateDomainsJSON(unittest.TestCase):
     def test_perform(self):
-        json = CreateDomainsJSON('tests/scripts/a11y/data/domains.csv',
+        domainsJSON = CreateDomainsJSON('tests/scripts/a11y/data/domains.csv',
                                  'tests/scripts/a11y/data/a11y.csv').perform()
-        print(json)
+        print(domainsJSON)
 
 if __name__ == '__main__':
     unittest.main()
+
+"""
+
+{
+  "data": [
+    {
+      "agency": "General Services Administration",
+      "branch": "Executive",
+      "canonical": "http:\/\/18f.gov",
+      "domain": "18f.gov",
+      "errors": 0,
+      "errorlist": {
+        "Alt Tag Errors": 0,
+        "Color Contrast Errors": 0,
+        "HTML\/Attribute Errors": 0
+      }
+    }
+  ]
+}
+
+"""
