2e9d308fa981ab6ab6c6625f2f39c6c238e29b47
Alan deLevie <adelevie@gmail.com>
2016-09-13 16:34:40 -0400

    stopping point

--- scripts/a11y/create_domains_file.py
+++ scripts/a11y/create_domains_file.py
@@ -24,23 +24,48 @@ class Base:
 
 class CreateDomainsJSON(Base):
     def __init__(self, domainsCsvPath, a11yCsvPath):
-        self._domainsCsvPath = domainsCsvPath
-        self._a11yCsvPath = a11yCsvPath
+        self._a11yCsv = self._readCsv(a11yCsvPath)
+        self._domainsCsv = self._readCsv(domainsCsvPath)
 
     def perform(self):
-        domainsCsv = self._readCsv(self._domainsCsvPath)
-        a11yCsv = self._readCsv(self._a11yCsvPath)
-        return self._buildDomainsJSON(domainsCsv, a11yCsv)
+        return self._buildDomainsJSON(self._domainsCsv, self._a11yCsv)
 
     def _buildDomainsJSON(self, domainsCsv, a11yCsv):
         # format for each row in a11yCsv:
         # Domain,Base Domain,redirectedTo,typeCode,code,message,context,selector
-        result = defaultdict(list)
+        domains = defaultdict(list)
         for error in a11yCsv:
-            result[error[0]].extend(error[3:])
-
-        data = []
+            domains[error[0]].extend(error[3:])
 
         output = {}
-        output['data'] = data
-        return output
+        output['data'] = [self._createDomainDict(domain, domains[domain]) for domain in domains]
+        return json.dumps(output)
+
+    def _createDomainDict(self, domain, errorList):
+        return {
+            "agency": self._agencyFromDomain(domain),
+            "branch": self._branchFromDomain(domain),
+            "canonical": domain,
+            "errors": len(errorList),
+            "errorList": {
+                "Alt Tag Errors": self._altTagErrorCountsFromErrorList(errorList),
+                "Color Contrast Errors": self._colorContrastErrorCountsFromErrorList(errorList),
+                "HTML Attribute Errors": self._HTMLAttributeErrorCountsFromErrorList(errorList)
+            }
+        }
+
+    def _agencyFromDomain(self, domain):
+        print(len(self._a11yCsv))
+        return 'fakeAgency'
+
+    def _branchFromDomain(self, domain):
+        return 'fakeBranch'
+
+    def _altTagErrorCountsFromErrorList(self, errorList):
+        return 1
+
+    def _colorContrastErrorCountsFromErrorList(self, errorList):
+        return 1
+
+    def _HTMLAttributeErrorCountsFromErrorList(self, errorList):
+        return 1
