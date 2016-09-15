608e103e9f9fa6e997d4f8d94af22a73b5160647
Alan deLevie <adelevie@gmail.com>
2016-09-15 11:34:16 -0400

    pre-lunch stopping point

--- tests/scripts/a11y/test_create_domains_json.py
+++ tests/scripts/a11y/test_create_domains_json.py
@@ -6,29 +6,14 @@ from scripts.a11y.create_domains_file import CreateDomainsJSON
 class TestCreateDomainsJSON(unittest.TestCase):
     def test_perform(self):
         domainsJSON = CreateDomainsJSON('tests/scripts/a11y/data/domains.csv',
-                                 'tests/scripts/a11y/data/a11y.csv').perform()
-        print(domainsJSON)
+                                        'tests/scripts/a11y/data/a11y.csv').perform()
+
+
+        self.assertTrue('data' in domainsJSON)
+
+        data = domainsJSON
+
+        self.assertEqual(data.__class__, list)
 
 if __name__ == '__main__':
     unittest.main()
-
-"""
-
-{
-  "data": [
-    {
-      "agency": "General Services Administration",
-      "branch": "Executive",
-      "canonical": "http:\/\/18f.gov",
-      "domain": "18f.gov",
-      "errors": 0,
-      "errorlist": {
-        "Alt Tag Errors": 0,
-        "Color Contrast Errors": 0,
-        "HTML\/Attribute Errors": 0
-      }
-    }
-  ]
-}
-
-"""
