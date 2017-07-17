c04b2ec817e055724236ae226b5525d73e83356e
ultiferrago <tkh0006@auburn.edu>
2017-07-17 09:58:11 -0400

    Removed redundant headers.

--- scanners/trustymail.py
+++ scanners/trustymail.py
@@ -60,8 +60,7 @@ def scan(domain, options):
 
 
 headers = [
-    "Domain", "Base Domain", "Live",
-    "Sends Mail", "Mail Servers",
+    "Live", "Sends Mail", "Mail Servers",
     "SPF Record", "Valid SPF", "SPF Results",
     "DMARC Record", "Valid DMARC", "DMARC Results",
     "DMARC Record on Base Domain", "Valid DMARC Record on Base Domain", "DMARC Results on Base Domain",
