0f6a08a6dad6b0a90d43c93cf33caa41a7ad44c2
Eric Mill <eric@konklone.com>
2017-11-18 19:12:43 -0500

    clean up a11y scanner and convert to new format

--- scanners/a11y.py
+++ scanners/a11y.py
@@ -2,7 +2,6 @@ import json
 import logging
 import os
 import requests
-
 import yaml
 
 from scanners import utils
@@ -10,21 +9,12 @@ from scanners import utils
 
 workers = 3
 pa11y = os.environ.get("PA11Y_PATH", "pa11y")
-headers = [
-    "redirectedTo",
-    "typeCode",
-    "code",
-    "message",
-    "context",
-    "selector"
-]
 
 redirects = {}
-
 config = ""
 
 
-def init(options):
+def init(environment, options):
     global redirects
     global config
 
@@ -36,6 +26,7 @@ def init(options):
         if not redirects_file.endswith(".yml"):
             logging.error("--a11y_redirects should be a YML file")
             return False
+
         # if remote, try to download
         if redirects_file.startswith("http:") or redirects_file.startswith("https:"):
             redirects_path = os.path.join(utils.cache_dir(), "a11y_redirects.yml")
@@ -57,11 +48,13 @@ def init(options):
 
         with open(redirects_path, 'r') as f:
             redirects = yaml.load(f)
+
     # Get config
     if config_file:
         if not config_file.endswith(".json"):
             logging.error("--a11y_config should be a json file")
             return False
+
         # if remote, try to download
         if config_file.startswith("http:") or config_file.startswith("https:"):
             config_path = os.path.join(utils.cache_dir(), "a11y_config.json")
@@ -75,6 +68,74 @@ def init(options):
 
         config = config_path
     return True
+
+
+# Shell out to a11y and run the scan.
+def scan(domain, environment, options):
+    domain_to_scan = get_domain_to_scan(domain)
+
+    if (
+        (not domain_to_scan) or
+        utils.domain_is_redirect(domain) or
+        utils.domain_not_live(domain)
+    ):
+        logging.debug("\tSkipping a11y scan based on pshtt data.")
+        return None
+
+    errors = run_a11y_scan(domain)
+
+    return {
+        'scanned_domain': domain_to_scan,
+        'errors': errors
+    }
+
+
+def to_rows(data):
+    rows = []
+
+    for error in data['errors']:
+        rows.append([
+            data['scanned_domain'],
+            error['typeCode'],
+            error['code'],
+            error['message'],
+            error['context'],
+            error['selector']
+        ])
+
+    return rows
+
+headers = [
+    "redirectedTo",
+    "typeCode",
+    "code",
+    "message",
+    "context",
+    "selector"
+]
+
+
+def run_a11y_scan(domain):
+    command = [pa11y, domain, "--reporter", "json", "--level", "none", "--timeout", "300000"]
+
+    if config:
+        command += ["--config", config]
+
+    raw = utils.scan(command)
+
+    if not raw or raw == '[]\n':
+        results = [{
+            'typeCode': '',
+            'code': '',
+            'message': '',
+            'context': '',
+            'selector': '',
+            'type': ''
+        }]
+    else:
+        results = json.loads(raw)
+
+    return results
 
 
 def get_from_pshtt_cache(domain):
@@ -97,96 +158,3 @@ def get_domain_to_scan(domain):
         domain_to_scan = domain
 
     return domain_to_scan
-
-
-def get_a11y_cache(domain):
-    return utils.cache_path(domain, "a11y")
-
-
-def domain_is_cached(cache):
-    return os.path.exists(cache)
-
-
-def cache_is_not_forced(options):
-    return options.get("force", False) is False
-
-
-def cache_errors(errors, domain, cache):
-    cachable = json.dumps({'results': errors})
-    logging.debug("Writing to cache: %s" % domain)
-    content = cachable
-    destination = cache
-    utils.write(content, destination)
-
-
-def run_a11y_scan(domain, cache):
-    global config
-    logging.debug("[%s][a11y]" % domain)
-    pa11y = os.environ.get("PA11Y_PATH", "pa11y")
-    domain_to_scan = get_domain_to_scan(domain)
-    command = [pa11y, domain_to_scan, "--reporter", "json", "--level", "none", "--timeout", "300000"]
-    if config:
-        command += ["--config", config]
-    raw = utils.scan(command)
-    if not raw or raw == '[]\n':
-        results = [{
-            'typeCode': '',
-            'code': '',
-            'message': '',
-            'context': '',
-            'selector': '',
-            'type': ''
-        }]
-    else:
-        results = json.loads(raw)
-
-    cache_errors(results, domain, cache)
-
-    return results
-
-
-def get_errors_from_scan_or_cache(domain, options):
-    a11y_cache = get_a11y_cache(domain)
-    the_domain_is_cached = domain_is_cached(a11y_cache)
-    the_cache_is_not_forced = cache_is_not_forced(options)
-    logging.debug("the_domain_is_cached: %s" % the_domain_is_cached)
-    logging.debug("the_cache_is_not_forced: %s" % the_cache_is_not_forced)
-
-    # the_domain_is_cached: True
-    # the_cache_is_not_forced: False
-    results = []
-    if the_domain_is_cached and the_cache_is_not_forced:
-        logging.debug("\tCached.")
-        raw = open(a11y_cache).read()
-        data = json.loads(raw)
-        if not data.get('invalid'):
-            logging.debug("Getting from cache: %s" % domain)
-            results = data.get('results')
-    else:
-        logging.debug("\tNot cached.")
-        results = run_a11y_scan(domain, a11y_cache)
-    return results
-
-
-def scan(domain, options):
-    logging.debug("[%s][a11y]" % domain)
-
-    domain_to_scan = get_domain_to_scan(domain)
-    if (utils.domain_is_redirect(domain) or
-            utils.domain_not_live(domain) or
-            not domain_to_scan):
-        logging.debug("Skipping a11y scan for %s" % domain)
-        return None
-    logging.debug("Running scan for %s" % domain)
-    errors = get_errors_from_scan_or_cache(domain, options)
-
-    for data in errors:
-        logging.debug("Writing data for %s" % domain)
-        yield [
-            domain,
-            data['typeCode'],
-            data['code'],
-            data['message'],
-            data['context'],
-            data['selector']
-        ]
