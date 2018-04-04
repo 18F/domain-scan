9eddc4279967ed82606b2fcdd5657121f7b032cb
Eric Mill <eric.mill@gsa.gov>
2018-04-04 19:20:55 -0400

    add --output flag, had not been set for gather yet

--- utils/utils.py
+++ utils/utils.py
@@ -191,6 +191,10 @@ def build_gather_options_parser(services):
         "Override the 10 minute job timeout (specify in seconds) ",
         "(censys only).",
     ]))
+    parser.add_argument("--output", nargs=1, default=["./"], help="".join([
+        "Where to output the 'cache/' and 'results/' directories. ",
+        "Defaults to './'.",
+    ]))
     return parser
 
 
