09b282af02e5f1960b2b0402230a5d779e9bd997
timothy-spencer <timothy.spencer@gsa.gov>
2020-02-12 16:46:53 -0800

    generate list of URLs and domains referenced in the html

--- scanners/third_parties.py
+++ scanners/third_parties.py
@@ -57,7 +57,9 @@ def to_rows(data):
         str.join(" | ", data['nearby_domains']),
         str.join(" | ", data['nearby_urls']),
         str.join(" | ", data['known_services']),
-        str.join(" | ", data['unknown_services'])
+        str.join(" | ", data['unknown_services']),
+        str.join(" | ", data['page_urls']),
+        str.join(" | ", data['page_domains'])
     ]]
 
 
@@ -69,5 +71,7 @@ headers = [
     'Nearby Domains',
     'Nearby URLs',
     'Known Services',
-    'Unknown Services'
+    'Unknown Services',
+    'URLs on the page',
+    'Domains in the URLs on the page'
 ]
